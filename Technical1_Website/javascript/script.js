069eb2a7f0baa810f602f4fccb16abb64a6c7bef
NanoScythe04 <nashresurreccion40@gmail.com>
2026-01-12 21:35:18 +0800

    Technical1_Website

---
+++ Technical1_Website/javascript/script.js
@@ -0,0 +1,19 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let message = document.getElementById('message').value;
+    let responseElement = document.getElementById('response');
+
+    if(name === "" || message === "") {
+        responseElement.textContent = "Please fill in all fields.";
+        responseElement.style.color = "red";
+        return;
+    } else {
+        responseElement.textContent = "Thank you, " + name + "! Your message has been received.";
+        responseElement.style.color = "green";
+        document.getElementById('contactForm')
+        this.reset();
+    }
+});
